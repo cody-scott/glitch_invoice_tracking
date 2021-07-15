14cfc11aa6f8c2f241e3b0544a9758ac4edd9b6d
namur007 <jcodyscott@gmail.com>
2021-07-15 02:40:36 +0000

    secret

--- security.py
+++ security.py
@@ -6,6 +6,6 @@ import os
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_header(secret_key: str = Header(...)):
-    _check = os.getenv('secret_key')
+    _check = os.getenv('secret_key_hash')
     if not pwd_context.verify(secret_key, _check):
         raise HTTPException(status_code=400, detail="Wrong secret!")
